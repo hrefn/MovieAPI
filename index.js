534e0efc18ea13f079bb8182fcf5a5f3f72fe33a
hrefn <cmcintosh1000@gmail.com>
2022-05-03 11:29:18 -0500

    test

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const express = require('express'),
 app.use(bodyParser.json());
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'https://young-reef-69362.herokuapp.com']
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message), false)
+    }
+    return callback(null, true);
+  }
+}));
 
 const { check, validationResult } = require('express-validator');
 
