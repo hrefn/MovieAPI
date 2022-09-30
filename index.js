59e826493970de9b063e8c0756c4f814d21dee29
hrefn <cmcintosh1000@gmail.com>
2022-09-30 12:22:14 -0500

    fixed cors

--- index.js
+++ index.js
@@ -20,22 +20,24 @@ const cors = require('cors');
 
 let allowedOrigins = ['*']
 
-app.use(cors({
-  /**
-   * 
-   * @param origin 
-   * @param callback 
-   * @returns callback
-   */
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
-      return callback(new Error(message), false)
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors(
+//   {
+//   /**
+//    * 
+//    * @param origin 
+//    * @param callback 
+//    * @returns callback
+//    */
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+//       return callback(new Error(message), false)
+//     }
+//     return callback(null, true);
+//   }
+// }
+));
 
 const { check, validationResult } = require('express-validator');
 const res = require('express/lib/response');
